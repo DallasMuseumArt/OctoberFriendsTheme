ee0d2383bcb91d36d60e1f5f6fa805cd8acbba11
Kristen Arnold <karnold@dma.org>
2015-02-05 13:27:24 -0600

    scroll to top of page when saving profile info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 (function($) {
     $(document).ready(function() {
         $('#user-profile button').click(function(e) {
+            window.scrollTo(0, 0);
             return $('#user-profile').parsley().validate();
         });
     });
