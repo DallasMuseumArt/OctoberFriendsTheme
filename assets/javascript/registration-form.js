a4d8a91f264ddf2e806ba17c27d54a31b2f2a2a0
Kristen Arnold <karnold@dma.org>
2015-02-05 11:04:44 -0600

    Improved validation of registration form

--- assets/javascript/registration-form.js
+++ assets/javascript/registration-form.js
@@ -1,40 +1,5 @@
 (function($) {
 
-    $.fn.extend({
-        donetyping: function(callback,timeout){
-            timeout = timeout || 1e3; // 1 second default timeout
-            var timeoutReference,
-                doneTyping = function(el){
-                    if (!timeoutReference) return;
-                    timeoutReference = null;
-                    callback.call(el);
-                };
-            return this.each(function(i,el){
-                var $el = $(el);
-                // Chrome Fix (Use keyup over keypress to detect backspace)
-                // thank you @palerdot
-                $el.is(':input') && $el.on('keyup keypress',function(e){
-                    // This catches the backspace button in chrome, but also prevents
-                    // the event from triggering too premptively. Without this line,
-                    // using tab/shift+tab will make the focused element fire the callback.
-                    if (e.type=='keyup' && e.keyCode!=8) return;
-                    
-                    // Check if timeout has been set. If it has, "reset" the clock and
-                    // start over again.
-                    if (timeoutReference) clearTimeout(timeoutReference);
-                    timeoutReference = setTimeout(function(){
-                        // if we made it here, our timeout has elapsed. Fire the
-                        // callback
-                        doneTyping(el);
-                    }, timeout);
-                }).on('blur',function(){
-                    // If we can, fire the event since we're leaving the field
-                    doneTyping(el);
-                });
-            });
-        }
-    });
-
     /** Carousel **/
     $('.registration-prev, .register').hide();
 
@@ -43,18 +8,19 @@
         wrap: false
     });
 
-    checkRequired($('#carousel-register .item.active'));
+    checkButtons($('#carousel-register .item.active'));
 
     $('#carousel-register').on('slid.bs.carousel', function(e) {
-        $('.registration-next').addClass('disabled');
-        checkRequired($('#carousel-register .item.active'));
+        checkButtons($('#carousel-register .item.active'));
 
-        if (e.direction == 'right' && false !== $('#register').parsley().validate('block' + $('.item.active').index())) {
-            $('.registration-next').removeClass('disabled');
-        }
     });
 
-    function checkRequired(obj) {
+    $('.registration-next').click(function(e) {
+        var idx = $('.item.active').index();
+        return $('#register').parsley().validate('block' + idx);
+    })
+
+    function checkButtons(obj) {
 
         var idx = $('.item.active').index();
 
@@ -71,25 +37,6 @@
             $('.register').hide();
             $('.registration-next').show();
         }
-
-        if (obj.find('div.is-required').length === 0) {
-            $('.registration-next').removeClass('disabled');
-            return;
-        }
-
-        obj.find('input, select').donetyping(function(e) {
-            var requiredFieldsComplete = false;
-
-            if (false !== $('#register').parsley().validate('block' + idx)) {
-                requiredFieldsComplete = true;
-            }
-
-            if (requiredFieldsComplete) {
-                $('.registration-next').removeClass('disabled');
-            } else {
-                $('.registration-next').addClass('disabled');
-            }
-        }, 2000);
     
     }
 
@@ -98,7 +45,6 @@
         selected: function( event, ui ) {
             var fileName = $(ui.selected).find('img').attr('src');
             $('#avatar').val(fileName);
-            $('.registration-next').removeClass('disabled');
         }
     });
 
