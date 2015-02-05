adeb3ff188ba7a350e24332e441d842284d5ce95
Kristen Arnold <karnold@dma.org>
2015-02-05 13:22:53 -0600

    moved parsley into theme for use in all forms.  also added support for user profile validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
 (function($) {
-
+    $(document).ready(function() {
+        $('#user-profile button').click(function(e) {
+            return $('#user-profile').parsley().validate();
+        });
+    });
 })(jQuery);
