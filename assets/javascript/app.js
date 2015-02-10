3c1635b603d45ba2caeb7f843eba9f83549eb599
Kristen Arnold <karnold@dma.org>
2015-02-10 10:37:13 -0600

    removed scrollTo in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 (function($) {
     $(document).ready(function() {
         $('#user-profile button').click(function(e) {
-            window.scrollTo(0, 0);
             return $('#user-profile').parsley().validate();
         });
     });
