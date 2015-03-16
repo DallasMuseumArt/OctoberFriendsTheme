cf2410d612a8bd8c6d2183d556e97d851ea8b5f3
Kristen Arnold <karnold@dma.org>
2015-03-16 10:51:51 -0500

    updated loading indicator

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,5 +3,22 @@
         $('#user-profile button').click(function(e) {
             return $('#user-profile').parsley().validate();
         });
+
+        $('#status').fadeOut(); 
+        $('#preloader').delay(500).fadeOut('slow'); 
+        $('body').delay(400).css({'overflow':'visible'});
+
+        $(document).on('ajaxUpdate', function(){
+            console.log('Updated!');
+            $('#status').fadeOut(); 
+            $('#preloader').fadeOut('slow'); 
+        });
+
+        $('form').on('submit', function() {
+            console.log('submit sent');
+            $('#status').fadeIn();
+            $('#preloader').fadeIn('slow');
+        });
     });
+
 })(jQuery);
